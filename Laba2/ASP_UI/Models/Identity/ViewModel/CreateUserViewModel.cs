813bd8e2b116c596aa915ba80fb271954ed81ec2
MaxZay <maximzay2017@gmail.com>
2021-05-14 20:45:50 +0300

    feat: identity crud

---
+++ Laba2/ASP_UI/Models/Identity/ViewModel/CreateUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_UI.Models.Identity.ViewModel
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+}
