66e6c64ed1de06176aabee327372e2f074793eae
MaxZay <maximzay2017@gmail.com>
2021-05-15 00:34:06 +0300

    feat: add roles crud

---
+++ Laba2/ASP_UI/Controllers/Identity/Roles/RolesController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ASP_UI.Models.Identiy;
+using ASP_UI.Models.Identity.ViewModel;
+
+namespace ASP_UI.Controllers.Identity.Roles
+{
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult List() => View(_roleManager.Roles.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("List");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("List");
+        }
+
+        public IActionResult UserList() => View(_userManager.Users.ToList());
+    }
+}
